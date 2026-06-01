d1cea9c9630a25706c1af2caa8b2c8e632874885
Gian Pierre <gianpierre@Gians-MacBook-Air.local>
2026-06-01 12:08:32 -0500

    temp: add missing PCGE accounts 4611 and 4711

---
+++ add_accounts.py
@@ -0,0 +1,33 @@
+from app import create_app
+from app.models.accounting_account import AccountingAccount
+from app.extensions import db
+
+app = create_app()
+with app.app_context():
+    cuentas = [
+        AccountingAccount(
+            code='4611',
+            name='Prestamos de accionistas',
+            type='pasivo',
+            nature='acreedora',
+            currency='PEN',
+            parent_code='46'
+        ),
+        AccountingAccount(
+            code='4711',
+            name='Intereses por pagar',
+            type='pasivo',
+            nature='acreedora',
+            currency='PEN',
+            parent_code='47'
+        ),
+    ]
+    for c in cuentas:
+        existing = AccountingAccount.query.filter_by(code=c.code).first()
+        if not existing:
+            db.session.add(c)
+            print('Creada: {} - {}'.format(c.code, c.name))
+        else:
+            print('Ya existe: {}'.format(c.code))
+    db.session.commit()
+    print('Listo.')
